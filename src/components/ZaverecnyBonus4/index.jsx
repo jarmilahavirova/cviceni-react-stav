815b3de9fdc91609e2f8c24e45a4462b7fd6f420
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-08 16:29:30 +0200

    Bonus 4

--- src/components/ZaverecnyBonus4/index.jsx
+++ src/components/ZaverecnyBonus4/index.jsx
@@ -1,14 +1,32 @@
-import './style.css'
+import { useState } from "react";
+import "./style.css";
 /*
 Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních
   políček, vypiš do `<output></output>` jejich součet.
 */
 
 export const ZaverecnyBonus4 = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
   return (
-    <div className='bonus4'>
-      <input type="number" defaultValue="0" /> +{' '}
-      <input type="number" defaultValue="0" /> = <output>0</output>
+    <div className="bonus4">
+      <input
+        type="number"
+        defaultValue="0"
+        onChange={(event) => {
+          setA(Number(event.target.value));
+        }}
+      />{" "}
+      +{" "}
+      <input
+        type="number"
+        defaultValue="0"
+        onChange={(event) => {
+          setB(Number(event.target.value));
+        }}
+      />{" "}
+      = <output>{a + b}</output>
     </div>
-  )
-}
+  );
+};
