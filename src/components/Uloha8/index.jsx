f3121c2923a8be22d3e6280447e1726fd40f019a
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-08 15:58:52 +0200

    Úloha 8

--- src/components/Uloha8/index.jsx
+++ src/components/Uloha8/index.jsx
@@ -4,11 +4,25 @@ Zadání 2: Proměnnou vypiš místo `ano/ne/možná`.
 Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 */
 
+import { useState } from "react";
+
 export const Uloha8 = () => {
+  const [prsiVBrne, setprsiVBrne] = useState("možná");
+
+  const zmenPrseni = () => {
+    if (prsiVBrne === "možná") {
+      setprsiVBrne("ano");
+    } else if (prsiVBrne === "ano") {
+      setprsiVBrne("ne");
+    } else if (prsiVBrne === "ne") {
+      setprsiVBrne("možná");
+    }
+  };
+
   return (
     <>
-      <h3>Prší v Brně: ano/ne/možná</h3>
-      <button>změnit</button>
+      <h3>Prší v Brně: {prsiVBrne}</h3>
+      <button onClick={zmenPrseni}>změnit</button>
     </>
-  )
-}
+  );
+};
